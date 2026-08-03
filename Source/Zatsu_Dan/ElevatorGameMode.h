7bd7bf335bd1dcf5b8cf62c49b67d46eb096c79f
kaitomiyamoto1 <kaitomiyamoto1@gmail.com>
2026-08-03 18:48:34 +0900

    [add]クラス追加

---
+++ Source/Zatsu_Dan/ElevatorGameMode.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "ElevatorTalkGameModeBase.h"
+#include "ElevatorGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ZATSU_DAN_API AElevatorGameMode : public AElevatorTalkGameModeBase
+{
+	GENERATED_BODY()
+	
+};
