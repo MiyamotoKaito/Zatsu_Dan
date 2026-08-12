255171b7986e5b5ebf2b433bb83b5c1571280a21
kaitomiyamoto1 <kaitomiyamoto1@gmail.com>
2026-08-12 23:25:27 +0900

    [update]気まずいゲージの関数定義

--- Source/Zatsu_Dan/AwkwardGauge.cpp
+++ Source/Zatsu_Dan/AwkwardGauge.cpp
@@ -3,3 +3,10 @@
 
 #include "AwkwardGauge.h"
 
+void UAwkwardGauge::NativeTick(const FGeometry& Geometry, float DeltaTime)
+{
+	if (bIsStarted)
+	{
+		Super::NativeTick(Geometry, DeltaTime);
+	}
+}
--- Source/Zatsu_Dan/AwkwardGauge.h
+++ Source/Zatsu_Dan/AwkwardGauge.h
@@ -3,15 +3,30 @@
 #pragma once
 
 #include "CoreMinimal.h"
-#include "Blueprint/UserWidgetBlueprint.h"
+#include "Blueprint/UserWidget.h"
+#include "Components/ProgressBar.h"
 #include "AwkwardGauge.generated.h"
 
 /**
  * 気まずいゲージ
  */
-UCLASS()
-class ZATSU_DAN_API UAwkwardGauge : public UUserWidgetBlueprint
+UCLASS(Blueprintable)
+class ZATSU_DAN_API UAwkwardGauge : public UUserWidget
 {
 	GENERATED_BODY()
 	
+protected:
+	/**気まずいゲージのImage*/
+	UPROPERTY(meta = (BindWidget))
+	TObjectPtr<UProgressBar> ProgressBar;
+	/**最大値*/
+	UPROPERTY(Blueprintable, EditAnywhere, Category = "Stats")
+	float MaxValue;
+	/**減少速度*/
+	UPROPERTY(Blueprintable, EditAnywhere, Category = "Stats")
+	float DecreaseRate = 0.1f;
+	/**UIのTickに相当する関数*/
+	virtual void NativeTick(const FGeometry& Geometry, float DeltaTime)override;
+private:
+	bool bIsStarted = false;
 };
