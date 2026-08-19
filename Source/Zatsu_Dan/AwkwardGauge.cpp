4277e60ce12fda01cd8583babf12651dca368943
MiyamotoKaito <kaitomiyamoto1@gmail.com>
2026-08-19 19:55:35 +0900

    [update]デリゲート追加

--- Source/Zatsu_Dan/AwkwardGauge.cpp
+++ Source/Zatsu_Dan/AwkwardGauge.cpp
@@ -1,12 +1,36 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-
 #include "AwkwardGauge.h"
+
+void UAwkwardGauge::SetValue(float InMaxValue)
+{
+	MaxtValue = InMaxValue;
+	CurrentValue = MaxtValue;
+	UpdateProgressBar();
+}
 
 void UAwkwardGauge::NativeTick(const FGeometry& Geometry, float DeltaTime)
 {
-	if (bIsStarted)
+	Super::NativeTick(Geometry, DeltaTime);
+	
+	if (!bIsStarted)
 	{
-		Super::NativeTick(Geometry, DeltaTime);
+		return;
 	}
+	
+	CurrentValue -= DeltaTime;
+	
+	if (CurrentValue < 0.0f)
+	{
+		CurrentValue = 0.0f;
+		//　二重に発火するのを防ぐためにfalseに
+		bIsStarted = false;
+		// イベント発火
+		OnGaugeEmpty.Broadcast();
+	}
+	
+	UpdateProgressBar();
+}
+
+void UAwkwardGauge::UpdateProgressBar()
+{
+	ProgressBar->SetPercent(CurrentValue / MaxtValue);
 }
--- Source/Zatsu_Dan/AwkwardGauge.h
+++ Source/Zatsu_Dan/AwkwardGauge.h
@@ -7,6 +7,8 @@
 #include "Components/ProgressBar.h"
 #include "AwkwardGauge.generated.h"
 
+DECLARE_DYNAMIC_MULTICAST_DELEGATE(FOnGaugeEmpty);
+
 /**
  * 気まずいゲージ
  */
@@ -14,19 +16,30 @@ UCLASS(Blueprintable)
 class ZATSU_DAN_API UAwkwardGauge : public UUserWidget
 {
 	GENERATED_BODY()
-	
+public:
+	/**
+	 * 気まずいゲージの値を設定する
+	 * @param InMaxValue 
+	 */
+	UFUNCTION(BlueprintCallable, Category = "Stats")
+	void SetValue(float InMaxValue);
+	/** 気まずいゲージが０になったら発火するイベント(BluePrintで登録する)*/
+	UPROPERTY(BlueprintAssignable, Category = "Stats")
+	FOnGaugeEmpty OnGaugeEmpty;
 protected:
 	/**気まずいゲージのImage*/
 	UPROPERTY(meta = (BindWidget))
 	TObjectPtr<UProgressBar> ProgressBar;
 	/**最大値*/
 	UPROPERTY(Blueprintable, EditAnywhere, Category = "Stats")
-	float MaxValue;
+	float MaxtValue;
 	/**減少速度*/
 	UPROPERTY(Blueprintable, EditAnywhere, Category = "Stats")
 	float DecreaseRate = 0.1f;
 	/**UIのTickに相当する関数*/
 	virtual void NativeTick(const FGeometry& Geometry, float DeltaTime)override;
 private:
+	void UpdateProgressBar();
 	bool bIsStarted = false;
+	float CurrentValue;
 };
