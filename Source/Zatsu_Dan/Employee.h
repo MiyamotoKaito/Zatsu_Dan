3eac87f495d2dab162dedc0949c3c52af50d2e66
kaitomiyamoto1 <kaitomiyamoto1@gmail.com>
2026-08-25 07:34:06 +0900

    [add]社員クラス

---
+++ Source/Zatsu_Dan/Employee.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "SpeakerBase.h"
+#include "Employee.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ZATSU_DAN_API AEmployee : public ASpeakerBase
+{
+	GENERATED_BODY()
+	
+};
