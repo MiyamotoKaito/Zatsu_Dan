8eaea1cde6f6c3ddbedbcd204c2740326e6bf0ea
kaitomiyamoto1 <kaitomiyamoto1@gmail.com>
2026-08-25 07:10:42 +0900

    [add]社長アセット追加

---
+++ Source/Zatsu_Dan/President.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "ListenerBase.h"
+#include "President.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ZATSU_DAN_API APresident : public AListenerBase
+{
+	GENERATED_BODY()
+	
+};
