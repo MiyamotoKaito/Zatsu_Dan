e05e6ed8151bfca1b9bd7f8e923684de4dda5722
kaitomiyamoto1 <kaitomiyamoto1@gmail.com>
2026-08-25 07:06:14 +0900

    [add]タイトルゲームモードクラス

---
+++ Source/Zatsu_Dan/TitleGameMode.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "ElevatorTalkGameModeBase.h"
+#include "TitleGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ZATSU_DAN_API ATitleGameMode : public AElevatorTalkGameModeBase
+{
+	GENERATED_BODY()
+	
+};
