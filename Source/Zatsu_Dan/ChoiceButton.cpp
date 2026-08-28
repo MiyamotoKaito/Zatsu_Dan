e31ee2d01f5aa6741a1c8e4f850049c911c5a454
MiyamotoKaito <kaitomiyamoto1@gmail.com>
2026-08-28 20:32:38 +0900

    [add]クラス追加

---
+++ Source/Zatsu_Dan/ChoiceButton.cpp
@@ -0,0 +1,21 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "ChoiceButton.h"
+
+#include "Choice.h"
+
+UChoiceButton::UChoiceButton()
+{
+	OnClicked.AddDynamic(this, &UChoiceButton::HandleClicked);
+}
+
+FResult UChoiceButton::GetResult()
+{
+	return Result;
+}
+
+void UChoiceButton::HandleClicked()
+{
+	OnAwkwardGaugeEmpty.Broadcast(GetResult());
+}
---
+++ Source/Zatsu_Dan/ChoiceButton.h
@@ -0,0 +1,33 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Result.h"
+#include "Components/Button.h"
+#include "ChoiceButton.generated.h"
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnChoiceButtonClicked, FResult, result);
+
+/**
+ * クリックしたときにResultを返すボタン
+ */
+UCLASS()
+class ZATSU_DAN_API UChoiceButton : public UButton
+{
+	GENERATED_BODY()
+
+public:
+	UChoiceButton();
+	
+	UPROPERTY(BlueprintAssignable, Category = "TalkSystem")
+	FOnChoiceButtonClicked OnAwkwardGaugeEmpty;
+	
+	FResult GetResult();
+protected:
+	
+private:
+	void HandleClicked();
+	UPROPERTY(EditAnywhere, Category = "Result")
+	FResult Result;
+};
