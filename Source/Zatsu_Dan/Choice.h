2a3b4640d519517110b1da88a6b983b8ffb0fcc3
MiyamotoKaito <kaitomiyamoto1@gmail.com>
2026-08-28 20:32:31 +0900

    [add]クラス追加

---
+++ Source/Zatsu_Dan/Choice.h
@@ -0,0 +1,21 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "ChoiceButton.h"
+#include "Blueprint/UserWidget.h"
+#include "Choice.generated.h"
+
+/**
+ * 選択肢UIクラス
+ */
+UCLASS()
+class ZATSU_DAN_API UChoice : public UUserWidget
+{
+	GENERATED_BODY()
+public:
+	/* ボタンの配列 */
+	UPROPERTY(BlueprintType, EditAnywhere, Category = "Button")
+	TArray<UChoiceButton> ChoiceButtons;
+};
