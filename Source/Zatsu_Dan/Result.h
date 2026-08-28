b4af8561d745cd664b3e9905068a72c5e5666ffc
MiyamotoKaito <kaitomiyamoto1@gmail.com>
2026-08-28 20:32:55 +0900

    [add]構造体追加

---
+++ Source/Zatsu_Dan/Result.h
@@ -0,0 +1,27 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "Result.generated.h"
+
+/*
+ *　会話したあとの結果
+ */
+USTRUCT(BlueprintType)
+struct FResult
+{
+	GENERATED_BODY()
+	
+public:
+	FResult();
+	/* Listenerの返答 */
+	UPROPERTY(EditAnywhere)
+	FString Response;
+	
+	/* Buttonに表示されるテキスト */
+	UPROPERTY(EditAnywhere)
+	FString Text;
+
+	/* ゲージの変更値 */
+	UPROPERTY(EditAnywhere)
+	int32 Value;
+};
